c641e83ef61bd57648efb8d0b0d73ac8d7acc7d0
Navneet <navneetkumar.html@gmail.com>
2024-09-11 19:19:19 +0530

    add new photo on news section

--- src/components/testinomials/Inheadlines.jsx
+++ src/components/testinomials/Inheadlines.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import h1 from "../../assets/readers/headline1.jpg"
+import h2 from "../../assets/readers/headline2.jpg"
 
 const Headlines = () => {
   const images = [
-    // Add your image URLs here
-   h1
-    // Add the rest of the images
+   
+   h1,h2
+    
   ];
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h2 className="text-3xl font-bold text-center mb-8 "> "प्रोफेसर की डेयरी" In headlines </h2>
+      <h2 className="text-3xl font-bold text-center mb-8 "> अखबारों की नजर में 'प्रोफेसर की डायरी' </h2>
       <div className="lg:w-1/3 w-3/3 mx-auto">
         {images.map((image, index) => (
-          <div key={index} className="relative overflow-hidden shadow-lg rounded-lg">
+          <div key={index} className="relative overflow-hidden shadow-lg rounded-lg mb-8 ">
             <img
               src={image}
               alt={`Laxman Yadav's book ${index + 1}`}
